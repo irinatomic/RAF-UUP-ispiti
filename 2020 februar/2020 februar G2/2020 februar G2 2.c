ff1ca8ae3489fd59020cf342b8461cf07222bfc6
Irina Tomic <106466354+irinatomic@users.noreply.github.com>
2022-07-18 12:27:30 +0200

    Update 2020 februar G2 2.c
    Added the input string (example)

--- 2020 februar/2020 februar G2/2020 februar G2 2.c
+++ 2020 februar/2020 februar G2/2020 februar G2 2.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <string.h>
 
+/*
+Unos: nmXabcdefg zhj acXbbbbb sXcccccc
+Ispis: abncdmefng bbabbcb ccsccscc
+*/
+
 void modifikuj(char* token){
 
     char novi[50], levaSlova[50];
